06a82716a91775826e1fba98da540d4cc71a6a77
hebingqian <hebingqian@cheyipai.com>
2022-07-22 10:09:58 +0800

    feat: 更正 & 重启2.0.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,8 @@ const nav =  [{
     text: 'JavaScript',
     link: '/base/js/define'
 }, {
-    text: '扫文记录',
-    link: '/base/record/bagu1'
+    text: '练习',
+    link: '/base/record/test'
 }, {
     text: '札记',
     link: '/diary/summary'
@@ -47,15 +47,15 @@ const sidebar = {
         // title: '记录',
         collapsable: false,
         children: [{
+            title: '掌握自测',
+            path: 'test'
+        }, {
             title: '八股记录',
             path: 'bagu1'
-        }, {
-            title: '扫书记录',
-            path: 'link'
         }]
     }],
     '/diary/': [{
-        // title: '记录',
+        // title: '日记',
         collapsable: false,
         children: [{
             title: '随写',
@@ -69,8 +69,8 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: `icon.png` }]
     ],
-    title: '公主日记',
-    description: '公主填坑日记',
+    title: '填坑之路',
+    description: '填坑日记',
     base: '/', 
     themeConfig: {
         nav,
