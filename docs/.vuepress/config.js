f52a7e2e5179367c529cb3eeab852fb6182e84e4
wenny <wenny333333@163.com>
2024-01-29 18:06:37 +0800

    feat: 完善jit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,57 +14,32 @@ const sidebar = {
     '/base/js/': [{
         // title: 'js',
         collapsable: false,
-        children: [{
-            title: '解释型语言与编译过程',
-            path: 'define'
-        }, 
-        {
-            title: '作用域与闭包',
-            path: 'scope'
-        },
-        {
-            title: '执行上下文',
-            path: 'context'
-        }, 
-        {
-            title: '从浏览器->V8->JS的执行过程',
-            path: 'execute'
-        },
-        {
-            title: '事件循环',
-            path: 'eventLoop'
-        },  {
-            title: '原型',
-            path: 'proto'
-        }, {
-            title: 'this',
-            path: 'this'
-        }, {
-            title: '面向对象相关',
-            path: 'extend'
-        }]
+        children: [
+        { title: '解释型语言与编译过程', path: 'define'}, 
+        { title: 'V8是如何执行JS的', path: 'v8'},
+        { title: '作用域与闭包', path: 'scope'},
+        { title: '执行上下文', path: 'context'}, 
+        { title: '从浏览器->V8->JS的执行过程', path: 'execute'},
+        { title: '事件循环', path: 'eventLoop'},
+        { title: '原型', path: 'proto'},
+        { title: 'this', path: 'this'}, 
+        { title: '面向对象相关', path: 'extend'}]
     }],
     '/base/summary/': [{
         // title: '记录',
         collapsable: false,
-        children: [{
-            title: 'HTTP',
-            path: 'http'
-        }, {
-            title: '浏览器与内核',
-            path: 'browser'
-        }]
+        children: [
+            { title: 'HTTP', path: 'http'},
+            { title: '浏览器与内核', path: 'browser'}
+        ]
     }],
     '/base/record/': [{
         // title: '记录',
         collapsable: false,
-        children: [{
-            title: '掌握自测',
-            path: 'test'
-        }, {
-            title: '八股记录',
-            path: 'bagu1'
-        }]
+        children: [
+            { title: '掌握自测', path: 'test'},
+            { title: '八股记录', path: 'bagu1'}
+        ]
     }],
     '/': [''],
 };
